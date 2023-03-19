03f5a798f48f7e5ee140d6156606cffabce4739d
yazeedahmed <dee3ayahmed25@gmail.com>
2023-03-19 12:53:41 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    window.customerName= "BOB"
+
+}
+
+function setBestCustomer() {
+        bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+ 
+const leastFavoriteCustomer = "Zy";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Bob";
+}
